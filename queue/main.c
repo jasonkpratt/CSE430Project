25f99ca5c236a6974a3d813b2c5ba6d8f6bab1d6
jasonkpratt <jkpratt@asu.edu>
2015-02-01 03:28:38 -0700

    Initial Commit

---
+++ queue/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  queue
+//
+//  Created by Jason Pratt on 2/1/15.
+//  Copyright (c) 2015 Jason Pratt. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
